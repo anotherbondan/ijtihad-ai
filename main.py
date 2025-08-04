652f921a70a3b561de4005f39ec005eec9d5379e
anotherbondan <anandagautama3112@gmail.com>
2025-08-04 20:08:52 +0700

    feat: add theme to halal checker page

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
-async def read_home_page(request: Request):
+async def home_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/chatbot", response_class=HTMLResponse)
-async def read_chatbot_page(request: Request):
+async def chatbot_page(request: Request):
     return templates.TemplateResponse("Chatbot/index.html", {"request": request})
 
 @app.get("/halal-checker", response_class=HTMLResponse)
-async def read_halal_checker_page(request: Request):
+async def halal_checker_page(request: Request):
     return templates.TemplateResponse("HalalChecker/index.html", {"request": request})
+
+@app.get("/halal-checker/result", response_class=HTMLResponse)
+async def result_halal_checker_page(request: Request):
+    return templates.TemplateResponse("HalalChecker/result.html", {"request": request})

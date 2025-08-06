0612b76e6615bb5cee8c12b8717565e2ccd9dd2a
anotherbondan <anandagautama3112@gmail.com>
2025-08-06 11:48:08 +0700

    feat: create 404 page

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.exception_handlers import http_exception_handler
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -22,3 +24,7 @@ async def halal_checker_page(request: Request):
 @app.get("/halal-checker/result", response_class=HTMLResponse)
 async def result_halal_checker_page(request: Request):
     return templates.TemplateResponse("HalalChecker/result.html", {"request": request})
+
+@app.exception_handler(404)
+async def not_found_page(request: Request, exc: StarletteHTTPException):
+    return templates.TemplateResponse("404.html", {"request": request}, status_code=404)

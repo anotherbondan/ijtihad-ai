0eb8aab88409b60dd4c786014fbd9d07f98fab8b
anotherbondan <anandagautama3112@gmail.com>
2025-08-13 20:54:32 +0700

    feat: add service worker

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class User(Model):
     id = fields.UUIDField(pk=True, default=uuid.uuid4)
     username = fields.CharField(max_length=50, unique=True)
     password = fields.CharField(max_length=128)  
-    email = fields.CharField(max_length=100, unique=True)   
+    email = fields.CharField(max_length=100, unique=True)  
 
 class RoomChat(Model):
     id = fields.UUIDField(pk=True, default=uuid.uuid4)
